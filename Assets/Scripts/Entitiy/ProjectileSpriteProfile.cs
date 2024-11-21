0a4954fd480d889867c8977db0b85b8898084ab8
qazxsw1240 <qazxsw010528@gmail.com>
2024-11-22 05:57:21 +0900

    ProjectileSpriteProfile 추가

---
+++ Assets/Scripts/Entitiy/ProjectileSpriteProfile.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+namespace PunchGear.Entity
+{
+    [Serializable]
+    public class ProjectileSpriteProfile
+    {
+        [field: SerializeField]
+        public Sprite AssembleImage { get; private set; }
+
+        [field: SerializeField]
+        public Sprite DisassembleImage { get; private set; }
+    }
+}
