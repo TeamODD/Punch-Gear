a7bc36173197daf2c2cec9223b974043d7b40d7a
illiliililil <erroses12@gmail.com>
2024-11-23 20:52:19 +0900

    배경음악 파괴 불가 설정
    Add:
    - 배경음악 DontDestroyOnLoad 생성

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,56 +1,44 @@
+using System;
 using UnityEngine;
 
 namespace PunchGear
 {
     public class AudioManager : MonoBehaviour
     {
-        private static AudioManager instance; //넌 뭐니
         public AudioSource audioSource;
-        public AudioClip clip;
-        public static AudioManager Instance
+        private static AudioManager _instance; //넌 뭐니
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        private static void Initialize()
         {
-            get
+            _instance = FindFirstObjectByType<AudioManager>();
+            if(!_instance) // 오디오 매니저가 없으면 실행
             {
-                if (instance == null)
-                {
-                    Debug.LogError("AudioManager instance is null. Make sure it is in the scene.");
-                }
-                return instance;
+                DontDestroyOnLoad(_instance);
             }
         }
-        private void Awake()
-        {
-            // Singleton pattern
-            if (instance == null)
-            {
-                instance = this;
-                DontDestroyOnLoad(gameObject); // 이 오브젝트를 씬 전환 시 파괴되지 않도록 설정
-            }
-            else
-            {
-                Destroy(gameObject); // 이미 존재하는 인스턴스가 있을 경우 현재 오브젝트를 파괴
-            }
-        }
-        private void Start()
-        {
-            audioSource = GetComponent<AudioSource>();
 
-            audioSource.clip = this.clip;
-            audioSource.loop = true; // 반복 재생 설정
-            audioSource.volume = 0.5f;
-            audioSource.Play(); // 지속적으로 재생
 
-            AudioPause(false);
-            Time.timeScale = 1f;
-        }
+        //private void Start()
+        //{
+        //    audioSource = GetComponent<AudioSource>();
 
-        public void AudioPause(bool isPaused)
-        {
-            audioSource.pitch = isPaused ? 0f : 1f;
-        }
-        public void SetVolume(float volume)
-        {
-            audioSource.volume = volume; // 0.0에서 1.0 사이의 값
-        }
+        //    audioSource.clip = this.clip;
+        //    audioSource.loop = true; // 반복 재생 설정
+        //    audioSource.volume = 0.5f;
+        //    audioSource.Play(); // 지속적으로 재생
+
+        //    AudioPause(false);
+        //    Time.timeScale = 1f;
+        //}
+
+        //public void AudioPause(bool isPaused)
+        //{
+        //    audioSource.pitch = isPaused ? 0f : 1f;
+        //}
+        //public void SetVolume(float volume)
+        //{
+        //    audioSource.volume = volume; // 0.0에서 1.0 사이의 값
+        //}
     }
 }
