99adb000faa3a90be6b464e443fab523db88ede6
qazxsw1240 <qazxsw010528@gmail.com>
2024-11-23 10:25:20 +0900

    마우스 쿨다운 기능 추가

--- Assets/Scripts/Entitiy/PlayerAssembleController.cs
+++ Assets/Scripts/Entitiy/PlayerAssembleController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,10 +9,18 @@ namespace PunchGear.Entity
         private Player _player;
         private Dictionary<Projectile, IMouseInputAction> _mouseInputActionLookup;
 
+        [field: SerializeField]
+        public float AssembleCooldown { get; private set; }
+
+        private bool _isAssembleFrozen;
+        private bool _isDisassembleFrozen;
+
         private void Awake()
         {
             _player = GetComponent<Player>();
             _mouseInputActionLookup = new Dictionary<Projectile, IMouseInputAction>();
+            _isAssembleFrozen = false;
+            _isDisassembleFrozen = false;
         }
 
         private void Start()
@@ -19,10 +28,9 @@ namespace PunchGear.Entity
             ProjectileLauncher launcher = ProjectileLauncher.Instance;
             launcher.OnProjectileCreated.AddListener(projectile =>
             {
-                IMouseInputAction action = new MouseInputAction(projectile, _player);
+                IMouseInputAction action = new MouseInputAction(projectile, _player, this);
                 _mouseInputActionLookup[projectile] = action;
                 GloballyPlayerInputHandler.Instance.AddAction(action);
-                Debug.Log("Projectile launched");
             });
             launcher.OnProjectileDestroyed.AddListener(projectile =>
             {
@@ -32,15 +40,59 @@ namespace PunchGear.Entity
             });
         }
 
+        private void OnDisable()
+        {
+            StopAllCoroutines();
+        }
+
+        private void FreezeMouse(MouseAssembleAction assembleAction)
+        {
+            StartCoroutine(FreezeMouseCoroutine(assembleAction));
+        }
+
+        private IEnumerator FreezeMouseCoroutine(MouseAssembleAction assembleAction)
+        {
+            yield return new WaitForFixedUpdate();
+            if (assembleAction == MouseAssembleAction.Assemble)
+            {
+                _isAssembleFrozen = true;
+                Debug.Log("Assemble is frozen");
+            }
+            else
+            {
+                _isDisassembleFrozen = true;
+                Debug.Log("Disassemble is frozen");
+            }
+            yield return new WaitForSecondsRealtime(AssembleCooldown);
+            if (assembleAction == MouseAssembleAction.Assemble)
+            {
+                _isAssembleFrozen = false;
+                Debug.Log("Assemble is normal");
+            }
+            else
+            {
+                _isDisassembleFrozen = false;
+                Debug.Log("Disassemble is normal");
+            }
+        }
+
+        private enum MouseAssembleAction
+        {
+            Assemble,
+            Disassemble
+        }
+
         private class MouseInputAction : IMouseInputAction
         {
             private readonly Projectile _projectile;
             private readonly Player _player;
+            private readonly PlayerAssembleController _assembleController;
 
-            public MouseInputAction(Projectile projectile, Player player)
+            public MouseInputAction(Projectile projectile, Player player, PlayerAssembleController assembleController)
             {
                 _projectile = projectile;
                 _player = player;
+                _assembleController = assembleController;
             }
 
             public void OnMouseDown(MouseInputs inputs)
@@ -49,13 +101,15 @@ namespace PunchGear.Entity
                 {
                     return;
                 }
-                if (inputs == MouseInputs.Left)
+                if (inputs == MouseInputs.Left && !_assembleController._isDisassembleFrozen)
                 {
                     _projectile.Disassemble();
+                    _assembleController.FreezeMouse(MouseAssembleAction.Disassemble);
                 }
-                else if (inputs == MouseInputs.Right)
+                else if (inputs == MouseInputs.Right && !_assembleController._isAssembleFrozen)
                 {
                     _projectile.Assemble();
+                    _assembleController.FreezeMouse(MouseAssembleAction.Assemble);
                 }
             }
         }
