55c53384024096158509053af727bcc3e71afa86
qazxsw1240 <qazxsw010528@gmail.com>
2024-11-24 20:41:46 +0900

    카메라 스크립트 추가

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,10 +4,36 @@ namespace PunchGear
 {
     public class Main : MonoBehaviour
     {
-        [RuntimeInitializeOnLoadMethod]
-        private static void Initialize()
+        private Camera _camera;
+
+        private static float _targetWidth = 1920;
+        private static float _targetHeight = 1080;
+
+        private void Awake()
         {
-            Debug.Log("Main class loaded on the scene");
+            _camera = GetComponent<Camera>();
+
+            Rect rect = _camera.rect;
+            float scaleHeight = ((float)Screen.width / Screen.height) / (_targetWidth / _targetHeight);
+            float scaleWidth = 1 / scaleHeight;
+
+            if (scaleHeight < 1f)
+            {
+                rect.height = scaleHeight;
+                rect.y = (1f - scaleHeight) / 2;
+            }
+            else
+            {
+                rect.width = scaleWidth;
+                rect.x = (1 - scaleWidth) / 2;
+            }
+
+            _camera.rect = rect;
+        }
+
+        private void OnPreCull()
+        {
+            GL.Clear(true, true, Color.black);
         }
     }
 }
